33c6e72732d1b2681ce7a4262933f31d8ddc013c
김의현 <dmlgus1922@naver.com>
2023-02-09 22:03:23 +0900

    문제풀이: 23.02.09 / 백준
    실버 4

--- 백준/재귀/백준 10994.py
+++ 백준/재귀/백준 10994.py
@@ -1,45 +1,21 @@
-star1_num = 4 * 1 - 3
-
-star1 = ['' for _ in range(star1_num)]
-
-star1[0] += '*' * star1_num
-
-print(*star1)
-print()
-
-
-star2_num = 4 * 2 - 3
-star2 = ['' for _ in range(star2_num)]
-
-star2[0] += '*' * star2_num
-star2[1] += '*' + ' ' * (star2_num - 2) + '*'
-star2[2] += '*' + star1[0] + '*'
-star2[3] += '*' + ' ' * (star2_num - 2) + '*'
-star2[4] += '*' * star2_num
-
-print(*star2, sep = '\n')
-
-
 def stars(n):
     if n == 1:
         return ['*']
     
     pre_star = stars(n-1)
+    pre_star_num = 4 * (n - 1) - 3
+
     star_num = 4 * n - 3
     star = ['' for _ in range(star_num)]
     
-    star[0] += 
-        
-    
+    star[0] += '*' * star_num
+    star[1] += '*' + ' ' * (star_num - 2) + '*'
+    star[-2] += '*' + ' ' * (star_num - 2) + '*'
+    star[-1] += '*' * star_num
 
+    for i in range(pre_star_num):
+        star[i + 2] += '* ' + pre_star[i] + ' *'
 
-'''
-1 - 1
+    return star
 
-2 - 5
-
-3 - 9
-
-4 - 13
-23.02.08 푸는 중
-'''
+print(*stars(int(input())), sep = '\n')
